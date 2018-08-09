9ffadc84ca9d870e252e7663b2430b23db4a027c
Chris Brennand <chrisb@mws-10489.ad.datafoundry.com>
2018-08-09 10:48:14 -0500

    banner setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Don't let the internet browse you.</h1>
-    <p>Protect yourself online with the world's most powerful VPN.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Don't let the internet browse you.</h1>
+        <p>Protect yourself online with the world's most powerful VPN.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
+
   </div>
 )
 
